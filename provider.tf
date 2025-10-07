e7d69175c7e8e2e4aac191c561a91eda1403dc0e
Travis Logan <travis.logan@sage.com>
2025-10-07 16:37:31 +0100

    Added initial Azure provider configuration

---
+++ provider.tf
@@ -0,0 +1,13 @@
+# Initialise the Azure Provider soiurce and version being used
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
