11b30b84e90216192ffabd2995f52444b307355d
Travis Logan <travis.logan@sage.com>
2025-10-07 17:16:51 +0100

    Add Azure service plan configuration to main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,16 @@ resource "azurerm_resource_group" "resource_group" {
       owner = "Travis"
     }
 }
+
+resource "azurerm_service_plan" "service_plan" {
+  name = "asp-travis-sandbox"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = azurerm_resource_group.resource_group.location
+  os_type = "Windows"
+  sku_name = "B1"
+
+  tags = {
+    environment = "sandbox"
+    owner = "Travis"
+  }
+}
